0298db9421c2cfb610d6bee7872cb30d39fc0d42
WM Duberney <duberney112@gmail.com>
2024-05-08 20:25:32 -0500

    Add test

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const initialToDoList = [
   { id: 3, status: false, title: 'Mi Tercera Tarea' },
   { id: 4, status: true, title: 'Tarea finalizada' },
   { id: 5, status: false, title: 'Tarea por finalizar' },
+  { id: 6, status: false, title: 'Comprar leche' },
 ];
 // =============================================================================
 
--- src/App.test.js
+++ src/App.test.js
@@ -26,4 +26,13 @@ describe('App', () => {
     const listItemElement = screen.getByText('Mi Primera Tarea');
     expect(listItemElement).toBeDefined();
   });
+
+
+  it('Que contenga la tarea "Comprar leche"', () => {
+    render(<App />);
+
+    const listItemElement = screen.getByText('Comprar leche 1');
+    expect(listItemElement).toBeDefined();
+  });
+  
 });
